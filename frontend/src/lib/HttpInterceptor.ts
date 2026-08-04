514a838a7fec22bdb621c47df95aeec3aa20016f
Shubhajit Paul <shubhajitpaul717@gmail.com>
2026-08-05 01:45:16 +0530

    feat: enhance HttpInterceptor to handle token refresh on 401 errors and improve error handling

--- frontend/src/lib/HttpInterceptor.ts
+++ frontend/src/lib/HttpInterceptor.ts
@@ -1,8 +1,34 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const HttpInterceptor = axios.create({
 	baseURL: "http://localhost:8080",
 	withCredentials: true,
 });
+
+type CustomAxiosError = AxiosError & {
+	config: {
+		_retry: boolean;
+	};
+};
+
+// Refresh tokens
+HttpInterceptor.interceptors.response.use(
+	(response) => response,
+	async (error: CustomAxiosError) => {
+		const originalRequest = error.config;
+
+		try {
+			if (error.response?.status === 401 && !originalRequest?._retry) {
+				originalRequest._retry = true;
+
+				await HttpInterceptor.post("/auth/refresh");
+
+				return HttpInterceptor(originalRequest);
+			}
+		} catch {
+			window.location.href = "/login";
+		}
+	},
+);
 
 export default HttpInterceptor;

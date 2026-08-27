bdd991851045dc5608fedf05c0d84b4b55174a69
Shubhajit Paul <shubhajitpaul717@gmail.com>
2026-08-28 03:10:25 +0530

    feat: refactor message retrieval logic to enforce friendship check before accessing messages

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -2,25 +2,12 @@ import { StatusCodes } from "http-status-codes";
 import messageService from "../services/message.service.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/apiResponse.js";
-import friendRepository from "../repositories/friend.repository.js";
-import ApiError from "../utils/apiError.js";
 
 const getMessages = asyncHandler(async (req, res) => {
     const currentUserId = req.user?._id as string;
     const friendId = req.params.friendId as string;
 
-    const isFriend = await friendRepository.existsFriendship(currentUserId, friendId, "accepted");
-
-    if (!isFriend) {
-        throw new ApiError(
-            StatusCodes.FORBIDDEN,
-            "You cannot access messages from a user who is not your friend.",
-        );
-    }
-
-    const conversationKey = [currentUserId, friendId].sort().join(":");
-
-    const messages = await messageService.getMessagesByConversationKey(conversationKey);
+    const messages = await messageService.getMessagesByConversationKey(currentUserId, friendId);
 
     return res
         .status(StatusCodes.OK)
--- backend/src/services/message.service.ts
+++ backend/src/services/message.service.ts
@@ -1,6 +1,20 @@
+import { StatusCodes } from "http-status-codes";
+import friendRepository from "../repositories/friend.repository.js";
 import messageRepository from "../repositories/message.repository.js";
+import ApiError from "../utils/apiError.js";
 
-const getMessagesByConversationKey = async (conversationKey: string) => {
+const getMessagesByConversationKey = async (currentUserId: string, friendId: string) => {
+    const isFriend = await friendRepository.existsFriendship(currentUserId, friendId, "accepted");
+
+    if (!isFriend) {
+        throw new ApiError(
+            StatusCodes.FORBIDDEN,
+            "You cannot access messages from a user who is not your friend.",
+        );
+    }
+
+    const conversationKey = [currentUserId, friendId].sort().join(":");
+
     const messages = await messageRepository.findMessagesByConversationKey(conversationKey);
 
     return messages;
